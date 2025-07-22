330d8e4cc2f72cdd079d9a592a911fd5117d7757
medamine-glitch <139164967+medamine-glitch@users.noreply.github.com>
2025-07-23 00:24:08 +0100

    f
    f

--- back/settings.py
+++ back/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5h*wvn)l5v0hzlg=#lk4hrdzq1$(n2(-=dt7=ns@e$=++nh^v7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://xywas.pythonanywhere.com/']
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'leads.apps.LeadsConfig',
     'corsheaders',
+    'rest_framework',
     
 ]
 
